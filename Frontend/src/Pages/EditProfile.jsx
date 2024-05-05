7a2ffa6e5305e144ef1076016afc8309a378893c
adorque <160372920+adorque@users.noreply.github.com>
2024-05-05 16:52:09 -0700

    added more features onto EditProfile

--- Frontend/src/Pages/EditProfile.jsx
+++ Frontend/src/Pages/EditProfile.jsx
@@ -8,14 +8,34 @@ function EditProfile () {
 
   return (
     <div>
-      <h1>Input Name</h1>
-        <input
-        type="text"
-        value={name}
-        onChange={handleChange}
-        placeholder='Type Name...'
-        />
+      <h1>Edit Profile</h1>
+          <h4>Name: 
+          <input
+            type="text"
+            value={name}
+            onChange={handleChange}
+            placeholder='Current Name Displayed here?...'
+          />
+          <input type="submit" value='Save'/>
+          </h4>
         <p>You Typed: {name}</p>
+        
+        <h4>Bio
+        <input
+            type="text"
+            value={name}
+            onChane={handleChange}
+            placeholder="current bio displayed here..."
+          />
+          <input type="submit" value='Save'/>
+          </h4>
+        
+        <h4>Email:
+          <h5>Display Email...</h5>
+        </h4>
+        
+        <h4>Passworld:</h4>
+          <h5>Display Password with *** ??</h5>
     </div>
   );
 }
