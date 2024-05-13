b1c0dae5bd4408bfed7d7a9b8e2c146f7d5fb7af
adorque <160372920+adorque@users.noreply.github.com>
2024-05-12 20:15:06 -0700

    changes made to edit profile

--- Frontend/src/Pages/EditProfile.jsx
+++ Frontend/src/Pages/EditProfile.jsx
@@ -1,21 +1,58 @@
 import React, { useState} from 'react';
+import "./EditProfile.css";
   
 function EditProfile () {
     const [name, setName] = useState('');
-    const handleChange = (event) => {
+    const [bio, setBio] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleNameChange = (event) => {
         setName(event.target.value);
+    };
+
+    const handleBioChange = (event) => {
+      setBio(event.target.value);
+    };
+
+    const handleEmailChange = (event) => {
+      setEmail(event.target.value);
     };
 
   return (
     <div>
-      <h1>Input Name</h1>
+      <h1>Edit Profile</h1>
+          <h4>Name: 
+          <input
+            type="text"
+            value={name}
+            onChange={handleNameChange}
+            placeholder='Current Name Displayed here?...'
+          />
+          <input type="submit" value='Save'/>
+          </h4>
+
+        <h4>Bio
         <input
-        type="text"
-        value={name}
-        onChange={handleChange}
-        placeholder='Type Name...'
-        />
-        <p>You Typed: {name}</p>
+            type="text"
+            value={bio}
+            onChange={handleBioChange}
+            placeholder="current bio displayed here..."
+          />
+          <input type="submit" value='Save'/>
+          </h4>
+        
+        <h4>Email:
+          <input
+            type="text"
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="current email displayed here..."
+          />
+          <input type="submit" value='Save'/>
+        </h4>
+        
+        <h4>Passworld:</h4>
+          <h5>Display Password with *** ??</h5>
     </div>
   );
 }
