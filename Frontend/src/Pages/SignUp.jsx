3a10710ae632c153dd65b6d6be53e0f5aa71c276
jackdeye <jackdeye@ucla.edu>
2024-05-16 22:25:15 -0700

    Can now create users

--- Frontend/src/Pages/SignUp.jsx
+++ Frontend/src/Pages/SignUp.jsx
@@ -1,18 +1,40 @@
-import React, { useState } from 'react';
-//import "../styles/Login.css"
 
 export default function SignUp() {    
-    const handleSubmission = (event) => {
+    var username = '';
+    var password = '';
+    const handleSubmission = async (event) => {
       event.preventDefault(); 
-      const username = event.target.username.value;
-      const password = event.target.password.value;
+      username = event.target.username.value;
+      password = event.target.password.value;
       console.log("Created an Account");
       console.log("Username: " + username);
-      console.log("Password: " + username);
-      //try: 
-      //catch: 
-
+      console.log("Password: " + password);
+      try {
+        const formDataToSend = new FormData();
+        formDataToSend.append('username', username);
+        formDataToSend.append('password', password);
+  
+        const response = await fetch('http://localhost:5050/user/upload', {
+          method: 'POST',
+          credentials: 'include',
+          body: formDataToSend
+          //body: JSON.stringify(formData)
+        });
+  
+        if (response.ok) {
+          alert('User created saved successfully!');
+          // setFormData({ name: '', desc: '' }); // reset form
+        } else {
+          const errorText = await response.text();
+          console.error('Failed to save post:', errorText);
+          throw new Error('Failed to save post.');
+        }
+      } catch (error) {
+        console.error('Error submitting post:', error);
+        alert('Failed to save post.');
+      }
     };
+
   return(
     <div className='container'>
     <div className='header'>
