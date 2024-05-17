3177bbdd61478c0e14a2ab16175ad0796c65be36
adorque <160372920+adorque@users.noreply.github.com>
2024-05-16 22:01:52 -0700

    accept merge to main

--- Frontend/src/Pages/Item.jsx
+++ Frontend/src/Pages/Item.jsx
@@ -1,32 +1,18 @@
 import React from 'react';
-<<<<<<< HEAD
 import './Gallery.jsx';
 import '../styles/Item.css'
 import cat from "/Users/alyssaleung/CS35L/Unifit/Frontend/Subject.jpg"
 
-const Item = ({product}) => {
-  return (
-    <div key={product.id} className="product-item">
-        <h3>{product.name}</h3>
-        <img src={product.image} />
-        <p>{"Buy: " + product.buyPrice}</p>
-        <p>{"Rent: " + product.rentPrice}</p>
-=======
 
 const Item = ({ product }) => {
   return (
     <div className="product-item">
-      <img src={`data:image/jpeg;base64,${product.image}`} alt={product.name} />
+      <img src={cat} alt={product.name} />
       <h3>{product.name}</h3>
       <p>Buy Price: {product.buyPrice}</p>
       <p>Rent Price: {product.rentPrice}</p>
->>>>>>> origin/main
     </div>
   );
 };
 
-<<<<<<< HEAD
 export default Item;
-=======
-export default Item;
->>>>>>> origin/main
