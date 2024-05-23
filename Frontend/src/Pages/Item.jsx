5cbce348d0acc70f9f16fcf19a3e79dbf2f028aa
adorque <160372920+adorque@users.noreply.github.com>
2024-05-23 14:20:20 -0700

    added like button, still need to connect liking to backend

--- Frontend/src/Pages/Item.jsx
+++ Frontend/src/Pages/Item.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import '../styles/Item.css'; // Assuming you have a CSS file for styling
+import { useState } from "react";
 
 const Item = ({ product }) => {
+  const [like, setLike] = useState(false);
+
+  const handleLike = () => {
+    setLike(!like);
+    console.log(`set favorite to ${!like}`);
+  };
+
+
   return (
     <div className="product-item">
       <img src={`data:image/jpeg;base64,${product.image}`} alt={product.name} />
@@ -9,9 +18,18 @@ const Item = ({ product }) => {
       <p>Buy Price: {product.buyPrice}</p>
       <p>Rent Price: {product.rentPrice}</p>
       
-      <button class="heart-button" aria-label="Like">
+      <button
+        className={`heart-button ${like ? 'liked' : ''}`}
+        aria-label="Like"
+        onClick={handleLike}
+      >
+        <span className="heart"></span>
+      </button>
+
+
+      {/* <button class="heart-button" aria-label="Like">
         <span class="heart"></span>
-    </button>
+    </button> */}
     </div>
   );
 };
