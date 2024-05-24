3a4fcec466bcc106eb5bef2f060b183a8f40dc2a
Fiona Peng <fionapeng1616@gmail.com>
2024-05-24 01:29:24 -0700

    some auth changes

--- Backend/routes/postRoute.js
+++ Backend/routes/postRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import db from "../connection.js";
 import { ObjectId } from "mongodb";
 import multer from "multer";
+import auth from '../middleware/auth.js';
 
 // Router is an instance of the express router.
 const router = express.Router();
@@ -90,5 +91,34 @@ router.delete("/:id", async (req, res) => {
     res.status(500).send("Error deleting post");
   }
 });
+
+// Like a post by id
+router.patch("/:id/likePost", async (req, res) => {
+  try {
+    const query = { _id: new ObjectId(req.params.id) };
+
+    if (!req.userId) return res.json({ message: 'Unauthenticated' });
+
+    const post = await PostMessage.findById(id);
+
+    const index = post.likes.findIndex((id) => id === String(req.userId));
+    if (index === -1) {
+      //like the post
+      post.likes.push(req.userId);
+    } else {
+      //dislike a post
+      post.likes = post.likes.filter((id) => id !== String(req.userId));
+    }
+    let collection = await db.collection("posts");
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { mew: true});
+    let result = await collection.updateOne(query, updatedPost);
+    res.send(result).status(200);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error liking/disliking post");
+  }
+});
+
 
 export default router;
--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -3,6 +3,9 @@ import db from "../connection.js";
 import { ObjectId } from "mongodb";
 import multer from "multer";
 
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
 // Router is an instance of the express router.
 const router = express.Router();
 const upload = multer();
@@ -13,43 +16,45 @@ router.get("/", async (req, res) => {
     res.send(results).status(200);
   });
   
-  // Get a single user by id
-  router.get("/:id", async (req, res) => {
+  // SignIn
+router.get("/signin", async (req, res) => {
     let collection = await db.collection("users");
-    let query = { _id: new ObjectId(req.params.id) };
-    let result = await collection.findOne(query);
+    const existingUser = await collection.findOne({ email });
   
-    if (!result) res.send("Post not found").status(404);
-    else res.send(result).status(200);
-  });
+    if (!existingUser) res.send("User doesn't exist.").status(404);
 
-  // Create a new user
-router.post("/upload", upload.any(), async (req, res) => {
+    const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+    if (!isPasswordCorrect) return res.send("Invalid credentials.").status(400);
+
+    const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: "1h" });
+
+    res.status(200).json({ result: existingUser, token });
+});
+
+  // SignUp - create a new user
+router.post("/signup", upload.any(), async (req, res) => {
+    const { email, password, confirmPassword, firstName, lastName } = req.body;
+
     try {
-      if (!req.body.username || !req.body.password) {
-        return res.status(400).send("Username and password are required");
+      if (!email || !password || !firstName || !lastName) {
+        return res.status(400).send("Username, Password, First Name, and Last Name are required");
       }
-  
-      //const image = req.files.find(file => file.mimetype.startsWith('image/'));
-      /*if (!image) {
-        return res.status(400).send("Image file is required.");
-      }
-     */
+      const existingUser = await collection.findOne({ email });
+      if (existingUser) res.send("User already exists.").status(400);
 
-      //const base64Image = image.buffer.toString("base64");
-      const newDocument = {
-        username: req.body.username,
-        password: req.body.password,
-        //image: base64Image,
-      };
-  
+      if (passowrd !== confirmPassword) res.send("Passwords don't match.").status(400);
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const result = await User.create({ email, password: hashedPassword, name: '${firstName} ${lastName}' });
+      const token = jwt.sign({ email: result.email, id: result._id }, 'test', { expiresIn: "1h" });
+
+      res.status(201).json({ result, token });
       let collection = db.collection("users");
-      let result = await collection.insertOne(newDocument);
       
       res.sendStatus(204);
     } catch (err) {
       console.error(err);
-      res.status(500).send("Error adding user");
+      res.status(500).send("Something went wrong");
     }
   });
   
