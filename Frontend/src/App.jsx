ca481bbfcc7927b0c15a560394d485c3c8094407
Fiona Peng <fionapeng1616@gmail.com>
2024-05-30 17:08:31 -0700

    user auth changes fixed <3 <3 <3

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { jwtDecode } from 'jwt-decode';
+import {jwtDecode} from 'jwt-decode';
 import Homepage from './Pages/Homepage';
 import PostPage from './Pages/PostPage';
 import Profile from './Pages/Profile';
@@ -16,44 +16,56 @@ import ProtectedRoute from './Components/ProtectedRoute';
 export default function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('token'));
   const [profile, setProfile] = useState(localStorage.getItem('profile') || '');
+  const [profilePicture, setProfilePicture] = useState(localStorage.getItem('profilePicture') || '');
 
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (token) {
-      const decodedToken = jwtDecode(token);
-      const currentTime = Date.now() / 1000; // Convert to seconds
-      console.log("decodedToken.exp: ", decodedToken.exp);
-      console.log("currentTime: ", currentTime);
-      const timeUntilExpiration = decodedToken.exp - currentTime;
-      console.log("time until expiration", timeUntilExpiration);
-      
-      if (timeUntilExpiration <= 0) {
-        handleLogout();
-      } else {
-        const timeoutId = setTimeout(handleLogout, timeUntilExpiration * 1000);
-        return () => clearTimeout(timeoutId); // cleanup timeout on component unmount or token change
+      try {
+        const decodedToken = jwtDecode(token);
+        const currentTime = Date.now() / 1000; // Convert to seconds
+        const timeUntilExpiration = decodedToken.exp - currentTime;
+
+        if (timeUntilExpiration <= 0) {
+          handleLogout();
+        } else {
+          const timeoutId = setTimeout(handleLogout, timeUntilExpiration * 1000);
+          return () => clearTimeout(timeoutId); // Cleanup timeout on component unmount or token change
+        }
+      } catch (error) {
+        handleLogout(); // In case of any error in decoding the token
       }
     }
   }, [isAuthenticated]);
 
-  const handleLogin = (profile, token) => {
+  const handleLogin = (profile, token, profilePicture) => {
     localStorage.setItem('token', token);
     localStorage.setItem('profile', profile);
+    localStorage.setItem('profilePicture', profilePicture || ''); // Set profile picture
     setIsAuthenticated(true);
     setProfile(profile);
+    setProfilePicture(profilePicture || '');
+    // Dispatch custom event
+    const event = new Event('localStorageUpdated');
+    window.dispatchEvent(event);
   };
 
   const handleLogout = () => {
     localStorage.removeItem('token');
     localStorage.removeItem('profile');
+    localStorage.removeItem('profilePicture');
     setIsAuthenticated(false);
     setProfile('');
+    setProfilePicture('');
+    // Dispatch custom event
+    const event = new Event('localStorageUpdated');
+    window.dispatchEvent(event);
   };
 
   return (
     <BrowserRouter>
       <div>
-        <NavBar profile={profile} isAuthenticated={isAuthenticated} onLogout={handleLogout} />
+        <NavBar profile={profile} profilePicture={profilePicture} isAuthenticated={isAuthenticated} onLogout={handleLogout} />
         <Routes>
           <Route path="/homepage" element={<ProtectedRoute isAuthenticated={isAuthenticated}><Homepage /></ProtectedRoute>} />
           <Route path="/profile" element={<ProtectedRoute isAuthenticated={isAuthenticated}><Profile /></ProtectedRoute>} />
@@ -70,3 +82,77 @@ export default function App() {
     </BrowserRouter>
   );
 }
+
+
+// import { useState, useEffect } from 'react'
+// import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+// import { jwtDecode } from 'jwt-decode';
+// import Homepage from './Pages/Homepage';
+// import PostPage from './Pages/PostPage';
+// import Profile from './Pages/Profile';
+// import Login from './Pages/Login';
+// import Gallery from './Pages/Gallery';
+// import Favorites from './Pages/Favorites';
+// import NavBar from './Components/NavBar';
+// import EditProfile from './Pages/EditProfile';
+// import SignUp from './Pages/SignUp.jsx';
+// import ItemPage from './Pages/ItemPage';
+// import ProtectedRoute from './Components/ProtectedRoute';
+
+// export default function App() {
+//   const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('token'));
+//   const [profile, setProfile] = useState(localStorage.getItem('profile') || '');
+
+//   useEffect(() => {
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//       const decodedToken = jwtDecode(token);
+//       const currentTime = Date.now() / 1000; // Convert to seconds
+//       console.log("decodedToken.exp: ", decodedToken.exp);
+//       console.log("currentTime: ", currentTime);
+//       const timeUntilExpiration = decodedToken.exp - currentTime;
+//       console.log("time until expiration", timeUntilExpiration);
+      
+//       if (timeUntilExpiration <= 0) {
+//         handleLogout();
+//       } else {
+//         const timeoutId = setTimeout(handleLogout, timeUntilExpiration * 1000);
+//         return () => clearTimeout(timeoutId); // cleanup timeout on component unmount or token change
+//       }
+//     }
+//   }, [isAuthenticated]);
+
+//   const handleLogin = (profile, token) => {
+//     localStorage.setItem('token', token);
+//     localStorage.setItem('profile', profile);
+//     setIsAuthenticated(true);
+//     setProfile(profile);
+//   };
+
+//   const handleLogout = () => {
+//     localStorage.removeItem('token');
+//     localStorage.removeItem('profile');
+//     setIsAuthenticated(false);
+//     setProfile('');
+//   };
+
+//   return (
+//     <BrowserRouter>
+//       <div>
+//         <NavBar profile={profile} isAuthenticated={isAuthenticated} onLogout={handleLogout} />
+//         <Routes>
+//           <Route path="/homepage" element={<ProtectedRoute isAuthenticated={isAuthenticated}><Homepage /></ProtectedRoute>} />
+//           <Route path="/profile" element={<ProtectedRoute isAuthenticated={isAuthenticated}><Profile /></ProtectedRoute>} />
+//           <Route path="/gallery" element={<ProtectedRoute isAuthenticated={isAuthenticated}><Gallery /></ProtectedRoute>} />
+//           <Route path="/favorites" element={<ProtectedRoute isAuthenticated={isAuthenticated}><Favorites /></ProtectedRoute>} />
+//           <Route path="/postpage" element={<ProtectedRoute isAuthenticated={isAuthenticated}><PostPage /></ProtectedRoute>} />
+//           <Route path="/signup" element={<SignUp onSignup={handleLogin} />} />
+//           <Route path="/login" element={<Login onLogin={handleLogin} />} />
+//           <Route path="/editprofile" element={<ProtectedRoute isAuthenticated={isAuthenticated}><EditProfile /></ProtectedRoute>} />
+//           <Route path="/item/:id" element={<ProtectedRoute isAuthenticated={isAuthenticated}><ItemPage /></ProtectedRoute>} />
+//           <Route path="/" element={<Navigate to="/homepage" replace />} />
+//         </Routes>
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
--- Frontend/src/Components/NavBar.jsx
+++ Frontend/src/Components/NavBar.jsx
@@ -2,14 +2,17 @@ import { Link, useNavigate } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import "../styles/NavBar.css";
 
-export default function Navbar({ profile, isAuthenticated, onLogout }) {
+export default function Navbar({ profile, profilePicture, isAuthenticated, onLogout }) {
   const navigate = useNavigate();
-  const [profilePicture, setProfilePicture] = useState(localStorage.getItem('profilePicture'));
-  const [name, setName] = useState(localStorage.getItem('profile'));
+  const [localProfilePicture, setLocalProfilePicture] = useState(profilePicture);
+  const [name, setName] = useState(profile);
 
   const handleLogout = () => {
     onLogout();
     navigate('/login'); // Redirect to login page
+    // Dispatch custom event
+    const event = new Event('localStorageUpdated');
+    window.dispatchEvent(event);
   };
 
   const getProfileInitial = (name) => {
@@ -18,17 +21,26 @@ export default function Navbar({ profile, isAuthenticated, onLogout }) {
 
   useEffect(() => {
     const handleStorageChange = () => {
-      setProfilePicture(localStorage.getItem('profilePicture'));
+      setLocalProfilePicture(localStorage.getItem('profilePicture'));
       setName(localStorage.getItem('profile'));
     };
 
+    window.addEventListener('storage', handleStorageChange);
     window.addEventListener('localStorageUpdated', handleStorageChange);
+
+    handleStorageChange();
 
     // Clean up the event listener on component unmount
     return () => {
       window.removeEventListener('localStorageUpdated', handleStorageChange);
+      window.removeEventListener('storage', handleStorageChange);
     };
   }, []);
+
+  useEffect(() => {
+    setLocalProfilePicture(profilePicture);
+    setName(profile);
+  }, [profilePicture, profile]);
 
   return (
     <div className='navbar'>
@@ -42,9 +54,9 @@ export default function Navbar({ profile, isAuthenticated, onLogout }) {
       </div>
       {isAuthenticated ? (
         <div className="profile-container">
-          {profilePicture && profilePicture !== 'null' ? (
+          {localProfilePicture && localProfilePicture !== 'null' ? (
             <img
-              src={`data:image/jpeg;base64,${profilePicture}`}
+              src={`data:image/jpeg;base64,${localProfilePicture}`}
               alt="Profile"
               className="profile-picture"
               onClick={() => navigate('/profile')}
@@ -57,7 +69,7 @@ export default function Navbar({ profile, isAuthenticated, onLogout }) {
               {getProfileInitial(name)}
             </div>
           )}
-          <h5 className='button'>Welcome, {name}</h5> {/* Changed profile to name */}
+          <h5 className='button'>Welcome, {name}</h5>
           <h5><button onClick={handleLogout}>Logout</button></h5>
         </div>
       ) : (
