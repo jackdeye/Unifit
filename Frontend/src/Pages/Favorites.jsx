2b0135c8d27705e33d190fdd0881651060347bba
jackdeye <jackdeye@ucla.edu>
2024-06-01 20:05:57 -0700

    Merged

--- Frontend/src/Pages/Favorites.jsx
+++ Frontend/src/Pages/Favorites.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/Gallery.css'; 
+// import Item from './Item.jsx';
 import Fuse from 'fuse.js';
 import Item from '../Components/Item.jsx';
 
