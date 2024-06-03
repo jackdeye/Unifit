d136ebd1443e0a4f73ebe91fd16ca23cfcb7bade
adorque <160372920+adorque@users.noreply.github.com>
2024-06-02 22:51:16 -0700

    able to like AND unlike posts (css part still doesnt work :(   )

--- Backend/routes/postRoute.js
+++ Backend/routes/postRoute.js
@@ -298,6 +298,7 @@ router.post("/:id/comments", async (req, res) => {
 );
 
 router.patch("/:id/likepost", auth, async (req, res) => {
+  const {like} = req.body;
   try {
     const postId = req.params.id;
     const userId = req.userId;
@@ -311,15 +312,40 @@ router.patch("/:id/likepost", auth, async (req, res) => {
       return res.status(404).send("Post not found");
     }
 
-    await userCollection.updateOne(
-      { _id: new ObjectId(userId) },
-      { $push: { likedPosts: new ObjectId(postId) } }
+    const findLike = await userCollection.findOne(
+      { _id: new ObjectId(userId), likedPosts: { $in: [new ObjectId(postId)] } },
+      { projection: { _id: 1 } } // Only return the _id field to make the operation lighter
     );
+      
+    if (findLike){ //unlike
+      console.log(findLike);
+      console.log('unliked');
+      // res.status(200).send("unliked");
+      await userCollection.updateOne(
+        { _id: new ObjectId(userId) },
+        { $pull: { likedPosts: new ObjectId(postId) } }
+      );
+  
+      await postCollection.updateOne(
+        { _id: new ObjectId(postId) },
+        { $set: { liked: false } }
+      );
+    } else { //like
+      console.log(findLike);
+      console.log('liked');
 
-    await postCollection.updateOne(
-      { _id: new ObjectId(postId) },
-      { $set: { liked: true } }
-    );
+      await userCollection.updateOne(
+        { _id: new ObjectId(userId) },
+        { $push: { likedPosts: new ObjectId(postId) } }
+      );
+  
+      await postCollection.updateOne(
+        { _id: new ObjectId(postId) },
+        { $set: { liked: true } }
+      );
+  
+    }
+
 
     res.status(200).send("Post liked successfully");
   } catch (err) {
