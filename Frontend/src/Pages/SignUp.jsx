ed5f5360cef492c84cec27551183eff78fed1132
23clarej <23clarej@students.harker.org>
2024-06-04 00:35:49 -0700

    yay made selection pretty, should work??, still blue...

--- Frontend/src/Pages/SignUp.jsx
+++ Frontend/src/Pages/SignUp.jsx
@@ -13,7 +13,6 @@ import {
   Grid,
   Select,
   MenuItem,
-  NativeSelect,
 } from '@mui/material';
 import {
   Visibility,
@@ -49,7 +48,6 @@ export default function SignUp({ onSignup }) {
 
   const handleSubmission = async (event) => {
     event.preventDefault(); 
-
     try {
       const formDataToSend = new FormData();
       formDataToSend.append('firstName', formData.firstName);
@@ -106,9 +104,7 @@ export default function SignUp({ onSignup }) {
         <Grid item>
         <TextField
           sx={{ m: 1, width: '25ch' }}
-          // id="outlined-required"
           label="First Name"
-          // onChange={(e)=>username=e.target.value}
           type='firstName'
           id='firstName'
           name='firstName'
@@ -120,9 +116,7 @@ export default function SignUp({ onSignup }) {
         <Grid item>
         <TextField
           sx={{ m: 1, width: '25ch' }}
-          // id="outlined-required"
           label="Last Name"
-          // onChange={(e)=>username=e.target.value}
           type='lastName'
           id='lastName'
           name='lastName'
@@ -149,14 +143,13 @@ export default function SignUp({ onSignup }) {
         <OutlinedInput
           id='password'
           name='password'
-          // id="outlined-adornment-password"
           type={showPassword ? 'text' : 'password'}
           endAdornment={
             <InputAdornment position="end">
               <IconButton
                 aria-label="toggle password visibility"
                 onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}//WTF DOES THIGS DO
+                onMouseDown={handleMouseDownPassword}
                 edge="end"
               >
                 {showPassword ? <VisibilityOff /> : <Visibility />}
@@ -182,15 +175,6 @@ export default function SignUp({ onSignup }) {
           label="School"
           onChange={handleChange}
         >
-          {/* <option value=''>Select a school</option>
-          <option value='UCLA'>UCLA</option>
-          <option value='University of Maryland'>University of Maryland, College Park</option>
-          <option value='oocla'>oocla</option>
-          <option value='UC Berkeley'>UC Berkeley</option>
-          <option value='Florida State University, School of Circustry'>Florida State University, School of Circustry</option>
-          <option value='University of Spoiled Children'>University of Spoiled Children</option>
-          <option value='Stanford University'>Stanford University</option>
-          <option value='Some school in the midwest (love u eggert)'>Some school in the midwest (love u eggert)</option> */}
           <MenuItem value={"UCLA"}>UCLA</MenuItem>
           <MenuItem value={'University of Maryland'}>University of Maryland, College Park</MenuItem>
           <MenuItem value={'oocla'}>oocla</MenuItem>
@@ -213,76 +197,5 @@ export default function SignUp({ onSignup }) {
   </Grid>
     </Paper>
     </div>
-  //   <div className='container'>
-  //   <div className='header'>
-  //     <h1>Signup</h1>
-  //   </div>
-  //   <div className='input'>
-  //     <form onSubmit={handleSubmission}>
-  //       <label htmlFor='firstName'>First Name:</label>
-  //         <input
-  //             type='firstName'
-  //             id='firstName'
-  //             name='firstName'
-  //             placeholder='First Name'
-  //             value={formData.firstName} 
-  //             onChange={handleChange}
-  //             required
-  //       />
-  //       <label htmlFor='lastName'>Last Name:</label> 
-  //         <input
-  //             type='lastName'
-  //             id='lastName'
-  //             name='lastName'
-  //             placeholder='Last Name'
-  //             value={formData.lastName} 
-  //             onChange={handleChange}
-  //             required
-  //         />
-  //       <label htmlFor='Username'>Username:</label>
-  //       <input
-  //           type='text'
-  //           id='username'
-  //           name='username' 
-  //           placeholder='Enter your Username'
-  //           value={formData.username}
-  //           onChange={handleChange}
-  //           required
-  //       />
-  //       <label htmlFor='password'>Password:</label>
-  //       <input
-  //           type='password'
-  //           id='password'
-  //           name='password'
-  //           placeholder='Enter your password'
-  //           value={formData.password} 
-  //           onChange={handleChange}
-  //           required
-  //       />
-  //       <label htmlFor='school'>School:</label>
-  //       <div></div>
-  //       <select
-  //         //type='school'
-  //         id='school'
-  //         name='school'
-  //         value={formData.school}
-  //         onChange={handleChange}
-  //         >
-  //           <option value=''>Select a school</option>
-  //           <option value='UCLA'>UCLA</option>
-  //           <option value='University of Maryland'>University of Maryland, College Park</option>
-  //           <option value='oocla'>oocla</option>
-  //           <option value='UC Berkeley'>UC Berkeley</option>
-  //           <option value='Florida State University, School of Circustry'>Florida State University, School of Circustry</option>
-  //           <option value='University of Spoiled Children'>University of Spoiled Children</option>
-  //           <option value='Stanford University'>Stanford University</option>
-  //           <option value='Some school in the midwest (love u eggert)'>Some school in the midwest (love u eggert)</option>
-  //       </select>
-  //       <div></div>
-        
-  //       <button type='submit'>Signup</button>
-  //     </form>
-  //   </div>
-  // </div>
 );
 }
