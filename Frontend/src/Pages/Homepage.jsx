d35f969db3bf2bd3264e93ee488759ea3d44d5b3
jackdeye <jackdeye@ucla.edu>
2024-06-03 21:35:14 -0700

    Basic parallax scrolling implemented
    Still need css

--- Frontend/src/Pages/Homepage.jsx
+++ Frontend/src/Pages/Homepage.jsx
@@ -1,41 +1,45 @@
 import React from 'react';
-import '../styles/Homepage.css';
-import { Link, useNavigate } from 'react-router-dom';
+import ParallaxSection from '../Components/ParallaxSection';
+import image1 from '../Assets/Yoink2.jpg';
+import image2 from '../Assets/Yoink3.jpg';
 
-const Homepage = () => {
-    const navigate = useNavigate();
+function Homepage() {
   return (
-    <div className="homepage">
+    <div>
+      <ParallaxSection image={image1}>
+        <h1>Welcome to <span style={{ color: "#367765" }}>Unifit</span></h1>
+        <p>Swap Sustainably</p>
+      </ParallaxSection>
+      <h3>Unifit: a platform that allows university students to effortlessly buy, sell, and rent clothes
+        or other dorm necessities on campus without the hassle of shipping over an item from a stranger.
+        Our goal is to fight against fast fashion, promote sustainability, and cultivate a close-knit community
+        on university campuses.</h3>
+      <div style={{ backgroundImage: `url(${image2})`, backgroundSize: 'cover', height: '500px' }}>
+        <h1>Section 2</h1>
+      </div>
       <div className='item'>
-        <h1>Unifit</h1> 
-        </div>
-        <div className='item'>
-        <p>
-          Swap Sustainably
-        </p>
-        </div>
-        <div className='item'>
         <button className='browse-gallery' onClick={() => navigate('/gallery')}>
           Browse Collection
         </button>
-        </div>
-        <div className='item'>
+      </div>
+      <div className='item'>
         <p> Welcome back! To access your profile or view your saved favorites, log in below.</p>
-          <p>New around here? Sign up below to create your account. </p>
-          </div>
-          <div className='item'>
+        <p>New around here? Sign up below to create your account. </p>
+      </div>
+      <div className='item'>
         <div className="start-button-container">
-        <button className="start-button" onClick={() => navigate('/login')}>
-          Login
-        </button>
-        <button className="start-button" onClick={() => navigate('/signup')}>
-          Sign up!
-        </button>
+          <button className="start-button" onClick={() => navigate('/login')}>
+            Login
+          </button>
+          <button className="start-button" onClick={() => navigate('/signup')}>
+            Sign up!
+          </button>
         </div>
       </div>
-
+      <p>More content here. You can add more parallax sections as needed.</p>
     </div>
   );
-};
+}
 
 export default Homepage;
+
